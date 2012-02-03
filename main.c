8dbfd4a7a33628be81adf057ddc3d59b61954654
Simon <srojasv10@gmail.com>
2012-02-02 20:33:36 -0430

    Main inicial

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(int argc,char* argv[]){
+  char *hostname;
+  int premoto, plocal, timeout;
+
+  if(argc == 9){
+
+  if (!strcmp(argv[1], "-h")) {
+    strcpy(hostname, argv[2]);
+      if (!strcmp(argv[3], "-p")) {
+        premoto = atoi(argv[4]);
+        if (!strcmp(argv[5], "-l")) {
+          plocal = atoi(argv[6]);
+          if (!strcmp(argv[7],"-t")) {
+            timeout = atoi(argv[8]);
+          }
+        }
+      }
+    }
+  } else {
+    perror("Se recomienda ejecutar nuevamente con el siguiente patron\nsiesta  -h <host psensor> -p <puerto psensor> -l <puerto local> -t <tiempo de ciclo>\n");
+    exit(1);
+  }
+  exit(0);
 }
